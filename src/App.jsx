bae9475224c003c739c5298e04fb35d1ea342deb
danieledgar <danieledgar097@gmail.com>
2025-11-18 04:05:44 +0300

    Fix routing: add basename for GitHub Pages and correct route paths

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,18 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
-import EducationPage from './pages/EducationPage';
 import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portofolio_website">
       <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white transition-colors duration-300">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
+          <Route path="/profile" element={<AboutPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
-          <Route path="/education" element={<EducationPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/resume" element={<ContactPage />} />
         </Routes>
         <Footer />
       </div>
